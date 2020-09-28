762e661393b2abd21eecfbd0533c1553f921a3e2
developerfromjokela <developerfromjokela@gmail.com>
2020-09-28 16:03:09 +0300

    Initializing project

---
+++ src/main/org/openkirkes/java/Library.java
@@ -0,0 +1,7 @@
+package org.openkirkes.java
+
+public class Library {
+    public boolean someLibraryMethod() {
+        return true;
+    }
+}
---
+++ src/test/org/openkirkes/java/test/LibraryTest.java
@@ -0,0 +1,11 @@
+package org.openkirkes.java.test
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class LibraryTest {
+    @Test public void testSomeLibraryMethod() {
+        Library classUnderTest = new Library();
+        assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
+    }
+}
